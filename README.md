1e8a1e9903db7a94a0b2141cbddba0d7601e087f
linkwisdom <linkwisdom@qq.com>
2013-12-29 17:56:35 +0800

    init codes

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# fcpipe 使用说明
+
+## 安装方法
+ * npm install fcpipe
+
+
+## 使用方法
+ * 1. 绑定客户端host 
+ *     127.0.0.1 fc-offline.baidu.com
+ * 2. 启动代理服务器
+ *    fcpipe start
+ *    或者基于源码
+ *    node pipe.js
+ 
---
+++ pipe.js
@@ -0,0 +1,87 @@
+/**
+ * @author Lianodng Liu
+ *
+ * 凤巢业务前端mock联调程序
+ * @date 2013年12月29日 17:25:14
+ */
+
+var httpProxy = require('http-proxy');
+var http = require('http');
+
+var proxyList = require('./config');
+var router = require('./router');
+
+// 根据请求url动态解析代理参数
+function proxyPass(url) {
+    var config = {
+        host: 'fc-offline.baidu.com',
+        port: 8000
+    };
+
+    var pass = false;
+
+    proxyList.forEach(function(item, idx) {
+        if (pass) {
+            return;
+        } 
+
+        var path = item.path;
+        pass = (('string' != typeof path) && url.match(path))
+            || (('string' == typeof path) && url.indexOf(path) > -1);
+
+        if (pass) {
+            config.host = item.host;
+            config.port = item.port;
+            config.url = url;
+            
+        }
+
+        if (pass && item.replace) {
+            config.url = config.url.replace(item.replace[0], item.replace[1]);
+        }
+
+        if (pass && item.nostamp) {
+            config.url = config.url.replace(/_\d+/g, '');
+            console.log(idx, config.url);
+        }
+
+        if (item.host in router) {
+            config.host = router[item.host];
+            config.qhost = item.host + ':' + item.port;
+        }
+    });
+
+    return config;
+}
+
+var proxy = new httpProxy.RoutingProxy();
+
+/**
+ * 安装方法
+ * npm install fcpipe
+ * 
+ * 使用方法
+ * 1. 绑定客户端host 
+ *     127.0.0.1 fc-offline.baidu.com
+ * 2. 启动代理服务器
+ *    fcpipe start
+ *    或者基于源码
+ *    node pipe.js
+ */
+http.createServer(function(request, response) {
+    var proxyConfig = proxyPass(request.url);
+
+    // 伪造host
+    if (proxyConfig.qhost) {
+        var headers = request.headers;
+        headers.host = proxyConfig.qhost;
+    }
+
+    // 修改url
+    if (proxyConfig.url) {
+        request.url = proxyConfig.url;
+    }
+
+    // 处理代理请求
+    proxy.proxyRequest(request, response, proxyConfig);
+}).listen(8000);
